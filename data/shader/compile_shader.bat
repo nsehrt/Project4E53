10a260eace1edca10c58d4e005e49b22274a2b6e
Nicolai Sehrt <nicolai.sehrt@mni.thm.de>
2020-03-31 14:18:27 +0200

    compile shader bat

---
+++ data/shader/compile_shader.bat
@@ -0,0 +1,8 @@
+fxc "Default.hlsl" /T vs_5_1 /E "VS" /Fo "Default_vs.cso"
+fxc "Default.hlsl" /T ps_5_1 /E "PS" /Fo "Default_ps.cso"
+
+fxc "Sky.hlsl" /T vs_5_1 /E "VS" /Fo "Sky_vs.cso"
+fxc "Sky.hlsl" /T ps_5_1 /E "PS" /Fo "Sky_ps.cso"
+
+fxc "Hitbox.hlsl" /T vs_5_1 /E "VS" /Fo "Hitbox_vs.cso"
+fxc "Hitbox.hlsl" /T ps_5_1 /E "PS" /Fo "Hitbox_ps.cso"
